5d53d5218d348e0b62da74a171d7c86a16107d4a
Korossy Ádám <h983772@stud.u-szeged.hu>
2022-03-31 18:54:42 +0200

    progress towards issue #12

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,48 +1,57 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+   selector: 'app-login',
+   templateUrl: './login.component.html',
+   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  public isSignedIn = false;
+   email = new FormControl('', [Validators.required, Validators.email]);
 
-  constructor(private renderer: Renderer2, public authService: AuthService) {
-   this.renderer.setStyle(document.body, 'background-image', 'url(assets/bcg1.jpg)');
-  }
+   public isSignedIn = false;
 
-  ngOnInit(): void {
-    if(localStorage.getItem('user') == null)
+   constructor(private renderer: Renderer2, public authService: AuthService) {
+      this.renderer.setStyle(document.body, 'background-image', 'url(assets/bcg1.jpg)');
+   }
+
+   ngOnInit(): void {
+      if (localStorage.getItem('user') == null)
+         this.isSignedIn = false;
+      else
+         this.isSignedIn = true;
+   }
+
+   ngOnDestroy(): void {
+      this.renderer.setStyle(document.body, 'background-image', '');
+   }
+
+   async signIn(email: string, password: string) {
+      await this.authService.signIn(email, password);
+
+      if (this.authService.isLoggedIn)
+         this.isSignedIn = true;
+   }
+
+   async signUp(email: string, password: string) {
+      await this.authService.signUp(email, password);
+
+      if (this.authService.isLoggedIn)
+         this.isSignedIn = true;
+   }
+
+   handleLogout() {
       this.isSignedIn = false;
-    else
-      this.isSignedIn = true;
-  }
+   }
 
-  ngOnDestroy(): void {
-   this.renderer.setStyle(document.body, 'background-image', '');
-  }
+   getErrorMessage() {
+      if (this.email.hasError('required')) {
+         return 'Kérlek add meg az e-mail címedet!';
+      }
 
-  async signIn(email: string, password: string){
-    console.log("...............");
-    await this.authService.signIn(email, password);
-    
-    if(this.authService.isLoggedIn)
-      this.isSignedIn = true;
-  }
-
-  async signUp(email: string, password: string){
-    console.log("SIGNZP");
-    await this.authService.signUp(email, password);
-    
-    if(this.authService.isLoggedIn)
-      this.isSignedIn = true;
-  }
-
-  handleLogout(){
-    this.isSignedIn = false;
-  }
+      return this.email.hasError('email') ? 'Nem valós e-mail cím.' : '';
+   }
 
 }
