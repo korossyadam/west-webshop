0a1b7cd7adbd434367edceac8d280c36c3db3f33
Korossy Ádám <h983772@stud.u-szeged.hu>
2022-04-11 19:27:27 +0200

    additional routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CarSelectorComponent } from './car-selector/car-selector.component';
+import { CartComponent } from './cart/cart.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -11,7 +12,8 @@ const routes: Routes = [
     { path: '', redirectTo: '/main', pathMatch: 'full' },
     { path: 'main', component: HomeComponent },
     { path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService] },
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'cart', component: CartComponent }
 ];
 
 @NgModule({
