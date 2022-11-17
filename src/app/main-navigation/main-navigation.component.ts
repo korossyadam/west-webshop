2ec547484b149b2568fe7a9bc6c0198ab5fa815c
Korossy Ádám <h983772@stud.u-szeged.hu>
2022-11-17 19:46:49 +0100

    Added new navigation system, needs more work

--- src/app/main-navigation/main-navigation.component.ts
+++ src/app/main-navigation/main-navigation.component.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from '../login/login.component';
 import { Product } from '../models/product.model';
 import { ProductsComponent } from '../products/products.component';
 import { AuthService } from '../services/auth.service';
+import { UtilsService } from '../services/utils.service';
 
 @Component({
    selector: 'app-main-navigation',
@@ -11,12 +12,15 @@ import { AuthService } from '../services/auth.service';
 })
 export class MainNavigationComponent implements OnInit {
 
+   @Output() carSelectClickedEvent = new EventEmitter<boolean>();
+   //@ViewChild(ProductsComponent) child: ProductsComponent;
+
+   // Static functions
+   sanitize = this.utilsService.sanitize;
+
    public step: number = 0;
    public secondHover: number = -1;
    public thirdHover: number = -1;
-
-   @Output() carSelectClickedEvent = new EventEmitter<boolean>();
-   @ViewChild(ProductsComponent) child: ProductsComponent;
 
    cartItems: Product[];
    cartItemsAmount: number[];
@@ -25,62 +29,68 @@ export class MainNavigationComponent implements OnInit {
 
    value = "";
    public searchedText: string;
-   public searchedSomething: boolean = false;
 
    isIf: boolean;
 
-   constructor(private authService: AuthService, private changeDetectorRef: ChangeDetectorRef) { }
+   constructor(private utilsService: UtilsService, private authService: AuthService, private changeDetectorRef: ChangeDetectorRef) { }
 
    ngOnInit(): void {
       this.cartItems = [];
       this.cartItemsAmount = [];
-
+      /*
       var localStorageCart = localStorage.getItem('cart');
-      var parts: string[] = localStorageCart?.split('/')!;
 
-      var total = 0;
-      if (parts[0] != 'undefined' && localStorageCart !== '') {
+      if (localStorageCart != null) {
+         var parts: string[] = localStorageCart?.split('/')!;
 
-         for (var i = 0; i < parts.length; i++) {
-            var attributes = parts[i].split('!');
+         var total = 0;
+         if (parts[0] != 'undefined' && localStorageCart !== '') {
 
-            var actualPrice = parseInt(attributes[3]) * this.cartItemsAmount[i];
+            for (var i = 0; i < parts.length; i++) {
+               var attributes = parts[i].split('!');
 
-            // Convert number to string
-            var price = this.formatPriceToString(actualPrice);
+               var actualPrice = parseInt(attributes[3]) * this.cartItemsAmount[i];
 
-            var product: Product = new Product(attributes[0], attributes[1], '', attributes[2], price, [], [], [], JSON.parse(attributes[4]), [attributes[5]]);
-            this.cartItems.push(product);
-            this.cartItemsAmount.push(parseInt(attributes[6]));
-            total += actualPrice;
+               // Convert number to string
+               var price = this.formatPriceToString(actualPrice);
+
+               var product: Product = new Product(attributes[0], attributes[1], '', [], attributes[2], price, [], [], JSON.parse(attributes[4]), [attributes[5]], []);
+               this.cartItems.push(product);
+               this.cartItemsAmount.push(parseInt(attributes[6]));
+               total += actualPrice;
+            }
          }
-      }
 
-      // Subtotal formatting
-      this.subtotal = this.formatPriceToString(total);
-      this.subtotalNet = this.formatPriceToString(total / 1.27);
+         // Subtotal formatting
+         this.subtotal = this.formatPriceToString(total);
+         this.subtotalNet = this.formatPriceToString(total / 1.27);
 
 
-      // PLACEHOLDER
-      var imgurl: string[] = ['https://ic-files-res.cloudinary.com/image/upload/v1/item/i9gyjzcudo3yria8eoir.jpg'];
-      var imgurl2: string[] = ['https://ic-files-res.cloudinary.com/image/upload/v1/item/afqdizxnrofc202wjcqa.jpg'];
+         // PLACEHOLDER
+         var imgurl: string[] = ['https://ic-files-res.cloudinary.com/image/upload/v1/item/i9gyjzcudo3yria8eoir.jpg'];
+         var imgurl2: string[] = ['https://ic-files-res.cloudinary.com/image/upload/v1/item/afqdizxnrofc202wjcqa.jpg'];
 
-      this.cartItems = [];
-      var p1 = new Product("CU40110", 'Légszűrő', 'desc', 'YATO', '2.525 Ft', [], [], [], false, imgurl)
-      this.cartItems.push(p1);
-      this.cartItemsAmount.push(2);
-      var p2 = new Product("F142-A", 'Kalapács', 'desc', 'FILTRON', '195 Ft', [], [], [], false, imgurl2)
-      this.cartItems.push(p2);
-      this.cartItemsAmount.push(1);
-      var p3 = new Product("AARM.S14.12", 'Xenon Fényszóró', 'desc', 'MANN-FILTER', '259.900 Ft', [], [], [], true, imgurl)
-      this.cartItems.push(p3);
-      this.cartItemsAmount.push(19);
-      this.subtotal = '74.265 Ft';
-      this.subtotalNet = '58.476 Ft';
-      // PLACEHOLDER
+         this.cartItems = [];
+         var p1 = new Product("CU40110", 'Légszűrő', 'desc', [], 'YATO', '2.525 Ft', [], [], false, imgurl, []);
+         this.cartItems.push(p1);
+         this.cartItemsAmount.push(2);
+         var p2 = new Product("F142-A", 'Kalapács', 'desc', [], 'FILTRON', '195 Ft', [], [], false, imgurl2, []);
+         this.cartItems.push(p2);
+         this.cartItemsAmount.push(1);
+         var p3 = new Product("AARM.S14.12", 'Xenon Fényszóró', 'desc', [], 'MANN-FILTER', '259.900 Ft', [], [], true, imgurl, []);
+         this.cartItems.push(p3);
+         this.cartItemsAmount.push(19);
+         this.subtotal = '74.265 Ft';
+         this.subtotalNet = '58.476 Ft';
+         // PLACEHOLDER
+         
+      }*/
+
+
 
    }
 
+   /*
    updatChildIf(): void {
       this.isIf = !this.isIf;
       this.changeDetectorRef.detectChanges();
@@ -89,9 +99,9 @@ export class MainNavigationComponent implements OnInit {
    }
 
    onSearch(): void {
-      this.searchedSomething = true;
-      this.child.search()
+      this.child.search();
    }
+   */
 
    onCarSelectorButtonClick(): void {
       this.carSelectClickedEvent.emit(true);
@@ -116,7 +126,7 @@ export class MainNavigationComponent implements OnInit {
 
    getName(): string {
       var lastName = localStorage.getItem('lastName');
-      var firstName = localStorage.getItem('firstName')
+      var firstName = localStorage.getItem('firstName');
       return lastName + ' ' + firstName;
    }
 
@@ -126,21 +136,6 @@ export class MainNavigationComponent implements OnInit {
          return email;
       else
          return '';
-   }
-
-   formatPriceToString(total: number): string {
-      var returnString = '';
-
-      if (total < 999)
-         returnString = total + ' Ft';
-      else if (total < 9999)
-         returnString = total.toString().substring(0, 1) + ' ' + total.toString().substring(1, total.toString().length) + ' Ft';
-      else if (total < 99999)
-         returnString = total.toString().substring(0, 2) + ' ' + total.toString().substring(2, total.toString().length) + ' Ft';
-      else if (total < 999999)
-         returnString = total.toString().substring(0, 3) + ' ' + total.toString().substring(3, total.toString().length) + ' Ft';
-
-      return returnString;
    }
 
 }
