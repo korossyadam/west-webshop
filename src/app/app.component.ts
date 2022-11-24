597c1fe346966241b773018340cd35c18db78af7
Korossy Ádám <h983772@stud.u-szeged.hu>
2022-11-24 14:42:33 +0100

    Deleted unnecessary changes to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +8,7 @@ import { Router } from '@angular/router';
 export class AppComponent {
   title = 'west-webshop';
 
-  constructor(private router: Router) {}
-
-  ngOnInit(){
-      this.router.navigate([''])
-  }
+  constructor() {}
 }
 
 
