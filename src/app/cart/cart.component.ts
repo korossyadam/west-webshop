8b73e18b856f7fdf3f43411d1dd1c060b077440b
Korossy Ádám <h983772@stud.u-szeged.hu>
2022-11-24 14:30:08 +0100

    Updated cart component, needs more work

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 import { MatDialog } from '@angular/material/dialog';
-import { first } from 'rxjs';
+import { Order, ShippingAddress, BillingAddress, OrderedProduct } from '../models/order.model';
 import { Product } from '../models/product.model';
-import { ProductsService } from '../services/products.service';
+import { CartService } from '../services/cart.service';
+import { UtilsService } from '../services/utils.service';
 
 @Component({
    selector: 'app-cart',
@@ -16,119 +18,241 @@ export class CartComponent implements OnInit {
    firstFormGroup: FormGroup;
    secondFormGroup: FormGroup;
 
-   @ViewChild('dialogRef') dialogRef!: TemplateRef<any>;
-   dialogList: string[] = [];
+   // Static functions
+   addTax = this.utilsService.addTaxToPrice;
+   formatPriceToString = this.utilsService.formatPriceToString;
+   sanitize = this.utilsService.sanitize;
 
-   cartItems: Product[];
-   cartItemsAmount: number[];
-   isCartItemUpdated: boolean[];
-   subtotal: string = '';
-   subtotalNet: string = '';
+   public cartItems = [];
+   public deliveryPrice: number = 0;
+   public total: number = 0;
 
-   trackByIdentifier = (index: number, item: Product) => item.partNumber;
+   // Radio button values
+   public isShippingChecked: boolean = false;
+   public isCashPaymentChecked: boolean = false;
 
-   constructor(private productService: ProductsService, private _formBuilder: FormBuilder, public dialog: MatDialog) { }
+   // If this variable is true, a loading spinner is displayed on first section
+   public initialLoading: boolean = true;
+
+   // Variables filled out by user
+   public name: string;
+   public email: string;
+   public phoneNumber: string;
+   public deliveryPostalCode: string;
+   public deliveryCity: string;
+   public deliveryStreet: string;
+   public comment: string;
+   public invoiceName: string;
+   public invoicePostalCode: string;
+   public invoiceCity: string;
+   public invoiceStreet: string;
+   public taxNumber: string;
+
+   constructor(private utilsService: UtilsService, private cartService: CartService, private _formBuilder: FormBuilder, public dialog: MatDialog) { }
 
    ngOnInit(): void {
       this.firstFormGroup = this._formBuilder.group({
-         firstCtrl: ['', Validators.required],
+         radioGroupControl: ['', Validators.required],
       });
       this.secondFormGroup = this._formBuilder.group({
          secondCtrl: ['', Validators.required],
       });
 
       this.fillCart();
+      this.calculateTotal();
    }
 
-   fillCart(): void {
+   /**
+    * Populates cartItems array from localStorage
+    */
+   async fillCart(): Promise<void> {
       this.cartItems = [];
-      this.cartItemsAmount = [];
 
-      var parts: string[] = localStorage.getItem('cart')?.split('/')!;
-
-      var total = 0;
-      if(parts[0] != 'undefined') {
-
-         for(var i = 0; i < parts.length; i++) {
-            var attributes = parts[i].split('!');
-
-            var actualPrice = parseInt(attributes[3]) * this.cartItemsAmount[i];
-
-            // Convert number to string
-            var price = this.formatPriceToString(actualPrice);
-
-            var product: Product = new Product(attributes[0], attributes[1], '', attributes[2], price, [], [], [], JSON.parse(attributes[4]), [attributes[5]]);
-            this.cartItems.push(product);
-            this.cartItemsAmount.push(parseInt(attributes[6]));
-            total += actualPrice;
-         }
+      let localStorageCart = localStorage.getItem('cart');
+      if (localStorageCart) {
+         this.cartItems = JSON.parse(localStorageCart);
       }
 
-      // Subtotal formatting
-      this.subtotal = this.formatPriceToString(total);
-      this.subtotalNet = this.formatPriceToString(total / 1.27);
-
-      
-      // PLACEHOLDER
-      var imgurl: string[] = ['https://ic-files-res.cloudinary.com/image/upload/v1/item/i9gyjzcudo3yria8eoir.jpg'];
-      var imgurl2: string[] = ['https://ic-files-res.cloudinary.com/image/upload/v1/item/afqdizxnrofc202wjcqa.jpg'];
-
-      this.cartItems = [];
-      var p1 = new Product("partNumber1", 'Légszűrő', 'desc', 'YATO', '2.525 Ft', [], [], [], false, imgurl)
-      this.cartItems.push(p1);
-      this.cartItemsAmount.push(2);
-      var p2 = new Product("F142-A", 'Kalapács', 'desc', 'FILTRON', '195 Ft', [], [], [], false, imgurl2)
-      this.cartItems.push(p2);
-      this.cartItemsAmount.push(1);
-      var p3 = new Product("AARM.S14.12", 'Xenon Fényszóró', 'desc', 'MANN-FILTER', '259.900 Ft', [], [], [], true, imgurl)
-      this.cartItems.push(p3);
-      this.cartItemsAmount.push(19);
-      this.subtotal = '74.265 Ft';
-      this.subtotalNet = '58.476 Ft';
-      // PLACEHOLDER
-
-      // Fill boolean array
-      this.isCartItemUpdated = new Array(this.cartItems.length).fill(false);
-
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      this.initialLoading = false;
    }
 
-   openedItem(index: number): void {
-      if(this.isCartItemUpdated[index])
+   /**
+    * Calculates the total of the order
+    * Needs to be re-calculated every time the cart changes (including shipping and payment info)
+    */
+   calculateTotal(): number {
+      this.deliveryPrice = 0;
+      this.total = 0;
+
+      // Calculate total price of all products to be ordered
+      for (let i = 0; i < this.cartItems.length; i++) {
+         this.total += this.cartItems[i].price * this.cartItems[i].quantity;
+      }
+
+      // Add shipping costs
+      if (this.isShippingChecked)
+         this.deliveryPrice += 1990;
+      if (this.isCashPaymentChecked)
+         this.deliveryPrice += 390;
+
+      this.total += this.deliveryPrice;
+
+      return this.total;
+   }
+
+   /**
+   * Increments the cart button value by 1
+   * Does not increment if incremented value would be higher than the avaible product stock
+   * 
+   * @param inputId The ID of the input we need to change the value of
+   * @param cartIndex The index of the product that we are changing the quantity of (in cartItems[] array)
+   */
+   incrementInputValue(inputId: string, cartIndex: number): void {
+      let element = (<HTMLInputElement>document.getElementById(inputId));
+      if (parseInt(element.value) >= 7)
          return;
 
-      this.productService.getProduct('products', this.cartItems[index].partNumber).pipe(first()).subscribe(data => {
-         this.cartItems[index] = data[0];  
-         this.dialogList = this.cartItems[index].factoryNumbers});
-         
-      this.isCartItemUpdated[index] = true;
+      element.value = (parseInt(element.value) + 1).toString();
+      this.cartItems[cartIndex].quantity += 1;
    }
 
+   /**
+    * Decrements the cart button value by 1
+    * Does not decrement if decremented value would be lower than 1
+    * 
+    * @param inputId The ID of the input we need to change the value of
+    * @param cartIndex The index of the product that we are changing the quantity of (in cartItems[] array)
+    */
+   decrementInputValue(inputId: string, cartIndex: number): void {
+      let element = (<HTMLInputElement>document.getElementById(inputId));
+      if (parseInt(element.value) <= 1)
+         return;
+
+      element.value = (parseInt(element.value) - 1).toString();
+      this.cartItems[cartIndex].quantity -= 1;
+   }
+
+   /**
+    * Deletes an item from the cart
+    * Modifies both cartItems[], and localStorage cart
+    * 
+    * @param index The index of the Product to remove in cartItems[]
+    */
    deleteCartItem(index: number): void {
+      this.cartItems.splice(index, 1);
+      this.calculateTotal();
 
+      localStorage.setItem('cart', JSON.stringify(this.cartItems));
    }
 
-   openDialog() {
-      const myTempDialog = this.dialog.open(this.dialogRef, { data: this.dialogList });
-      myTempDialog.afterClosed().subscribe((res) => {
-  
-        // Data back from dialog
-        console.log({ res });
-      });
-    }
+   /**
+    * Changes the display of 'Tax Number' input field
+    * 'height: 0px' used instead of 'display: none' for smoother animations
+    * 
+    * @param event Whether the checkbox is checked or not
+    */
+   showTaxNumberFields(event: MatCheckboxChange): void {
+      let container = <HTMLInputElement>document.getElementsByClassName('tax-number-container')[0];
 
-   formatPriceToString(total: number): string {
-      var returnString = '';
+      if (event.checked) {
+         container.style.height = '46px';
+      } else {
+         container.style.height = '0px';
+      }
 
-      if(total < 999)
-         returnString = total + ' Ft';
-      else if(total < 9999)
-         returnString = total.toString().substring(0, 1) + ' ' + total.toString().substring(1, total.toString().length) + ' Ft';
-      else if(total < 99999)
-         returnString = total.toString().substring(0, 2) + ' ' + total.toString().substring(2, total.toString().length) + ' Ft';
-      else if(total < 999999)
-         returnString = total.toString().substring(0, 3) + ' ' + total.toString().substring(3, total.toString().length) + ' Ft';
+      // Open invoice fields since Tax Number field is in invoice fields
+      this.showInvoiceFields();
+   }
 
-      return returnString;
+   /**
+    * Open up invoice input fields
+    */
+   showInvoiceFields(): void {
+      let container = <HTMLInputElement>document.getElementsByClassName('invoice-container')[0];
+      container.style.height = '138px';
+   }
+
+   /**
+    * Changes the display of invoice input fields
+    * 'height: 0px' used instead of 'display: none' for smoother animations
+    * 
+    * @param event Whether the checkbox is checked or not
+    */
+   changeInvoiceFieldsDisplay(event: MatCheckboxChange): void {
+      let container = <HTMLInputElement>document.getElementsByClassName('invoice-container')[0];
+
+      if (!event.checked) {
+         container.style.height = '138px';
+      } else {
+         container.style.height = '0px';
+      }
+   }
+
+   setUserInfo(name: string, email: string, phoneNumber: string, deliveryPostalCode: string, deliveryCity: string, deliveryStreet: string, comment: string,
+      invoiceName: string, invoicePostalCode: string, invoiceCity: string, invoiceStreet: string, taxNumber: string): void {
+      this.name = name;
+      console.log(this.name);
+      this.email = email;
+      this.phoneNumber = phoneNumber;
+      this.deliveryPostalCode = deliveryPostalCode;
+      this.deliveryCity = deliveryCity;
+      this.deliveryStreet = deliveryStreet;
+      this.comment = comment;
+      this.invoiceName = invoiceName;
+      this.invoicePostalCode = invoicePostalCode;
+      this.invoiceCity = invoiceCity;
+      this.invoiceStreet = invoiceStreet;
+      this.taxNumber = taxNumber;
+   }
+
+   /**
+    * Creates a new Order instance based on user input, and uploads it to Firestore
+    */
+   finalizeOrder(): void {
+
+      // Custom object for shipping address
+      let shippingAddress: ShippingAddress = {
+         name: this.name,
+         email: this.email,
+         phoneNumber: this.phoneNumber,
+         address: this.deliveryPostalCode + ', ' + this.deliveryCity + ', ' + this.deliveryStreet,
+         comment: this.comment,
+      };
+      
+      // Custom object for billing address
+      let billingAddress: BillingAddress = {
+         name: this.invoiceName,
+         address: this.invoicePostalCode + ', ' + this.invoiceCity + ', ' + this.invoiceStreet,
+         taxNumber: this.taxNumber,
+      };
+
+      // Fill Order with OrderedProducts
+      let orderedProducts: OrderedProduct[] = [];
+      for (let cartItem of this.cartItems) {
+         orderedProducts.push({ partNumber: cartItem.partNumber, name: cartItem.name, price: cartItem.price, quantity: cartItem.quantity, imgurl: cartItem.imgurls[0]})
+      }
+
+      // Stringify shipping and payment methods
+      let shippingMethod = 'Személyes átvétel';
+      if (this.isShippingChecked) {
+         shippingMethod = 'Házhozszállítás'
+      }
+
+      let paymentMethod = 'Előre utalás';
+      if (this.isCashPaymentChecked) {
+         paymentMethod = 'Utánvét';
+      }
+
+      // Re-calculate total price
+      this.calculateTotal();
+
+      let newOrder: Order = new Order('45hDYsjPsfXoT8X3JTF0Ekps7O32', new Date(), this.deliveryPrice, this.total, orderedProducts, shippingAddress, billingAddress, shippingMethod, paymentMethod);
+
+      this.cartService.addOrder(Object.assign({}, newOrder)).then(res => {
+         // move stepper forwards here
+      })
+
    }
 
 }
