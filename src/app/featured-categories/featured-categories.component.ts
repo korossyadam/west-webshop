e49c10c19fb2ca9ff108701d5bcbd105032e5c04
Korossy Ádám <h983772@stud.u-szeged.hu>
2023-01-29 14:01:39 +0100

    Optimized featured-categories component by reducing image sizes

--- src/app/featured-categories/featured-categories.component.ts
+++ src/app/featured-categories/featured-categories.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-featured-categories',
   templateUrl: './featured-categories.component.html',
   styleUrls: ['./featured-categories.component.css']
 })
-export class FeaturedCategoriesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class FeaturedCategoriesComponent {}
