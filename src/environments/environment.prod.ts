7c687154c59a3f2a05baba4a43e121d2ef249098
Korossy Ádám <h983772@stud.u-szeged.hu>
2023-01-30 21:37:01 +0100

    Firebase api key was added to production enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCIO_tHUe7fz7uyzrH2bSQAZKfin5lJpb8",
+    authDomain: "west-webshop.firebaseapp.com",
+    projectId: "west-webshop",
+    storageBucket: "west-webshop.appspot.com",
+    messagingSenderId: "219905200285",
+    appId: "1:219905200285:web:3a674f47694435caec8474",
+    measurementId: "G-XCYYHWBSY8"
+  },
 };
